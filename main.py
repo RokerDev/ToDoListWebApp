ce53f60e26248542a4c0f75db142f8245a1e3e3a
RokerDev <rokerdev91@gmail.com>
2022-03-20 11:38:50 +0100

    feat: add and configure flask sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////static/database/todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 
 @app.route("/")
