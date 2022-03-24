f772754dfa30948588d23d9b0399034970dd53d9
RokerDev <rokerdev91@gmail.com>
2022-03-24 19:31:26 +0100

    provide user_loader callback

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
