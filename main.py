142fccd4d79e2bfe2334ebac7cba8fe0e8885a72
RokerDev <rokerdev91@gmail.com>
2022-03-24 20:04:01 +0100

    import flask flash messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
