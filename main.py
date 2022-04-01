076a51dbe205210e9d82454809ec186e08a75c1a
RokerDev <rokerdev91@gmail.com>
2022-04-02 01:00:24 +0200

    feat: task status change
    add Status change form with hidden fields and status change button to table row
    add change status route changing task status
    edit other routes to display and handle task status change accordingly

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import datetime
 
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_wtf import FlaskForm
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from wtforms import StringField, SubmitField, PasswordField, SelectField, DateTimeField, BooleanField
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, SubmitField, PasswordField, SelectField, HiddenField
 from wtforms.fields.datetime import DateTimeLocalField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired
 
 LIST_OF_CATEGORIES = ["Home", "Shop", "Work", "Ideas", "Places"]
 LIST_OF_PERIODS = ["Today", "Month", "3 Month", "6 Month", "Year"]
@@ -82,9 +82,35 @@ class AddTaskForm(FlaskForm):
     submit = SubmitField("Add Task")
 
 
+class ChangeStatusForm(FlaskForm):
+    task_id = HiddenField()
+    url = HiddenField()
+    options = HiddenField()
+    submit = SubmitField()
+
+
 @app.route("/")
 def home():
     return render_template("index.html", current_user=current_user)
+
+
+@app.route("/change_status", methods=["GET", "POST"])
+def change_status():
+    if request.method == 'POST':
+        task_id = request.form['task_id'],
+        url = request.form['url']
+        options = request.form['options']
+        print(options)
+        print(url, options)
+        print("cos")
+        task = Tasks.query.get(task_id)
+        if task.check:
+            task.check = False
+        else:
+            task.check = True
+        db.session.commit()
+        return redirect(url_for(url, options=options))
+    return render_template("index.html")
 
 
 @app.route("/add_new_task", methods=["GET", "POST"])
@@ -94,7 +120,7 @@ def add_new_task():
 
     if form.validate_on_submit():
         task = Tasks(
-            author_task_id=len(current_user.task)+1,
+            author_task_id=len(current_user.task) + 1,
             check=False,
             description=form.descriptions.data,
             data=form.date.data,
@@ -110,56 +136,71 @@ def add_new_task():
 @app.route("/list_by_category/<options>")
 @login_required
 def user_todo_list_sorted_by_category(options):
+    form = ChangeStatusForm()
     user_tasks = current_user.task
     if options not in LIST_OF_CATEGORIES:
         return redirect(url_for("logout"))
-    tasks = [task for task in user_tasks if task.category == options and task.check == False]
+    tasks = [task for task in user_tasks if task.category == options and task.check is False]
 
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks, form=form,
+                           url="user_todo_list_sorted_by_category", options=options)
 
 
 @app.route("/list_by_period/<options>")
 @login_required
 def user_todo_list_sorted_by_periods(options):
     data = datetime.datetime.now()
+    form = ChangeStatusForm()
     user_tasks = current_user.task
     if options not in LIST_OF_PERIODS:
+        print(options)
         return redirect(url_for("logout"))
     if options == "Today":
-        tasks = [task for task in user_tasks if task.data.day == data.day]
+        tasks = [task for task in user_tasks if task.data.day == data.day and task.check is False]
     elif options == "Month":
-        tasks = [task for task in user_tasks if task.data.month == data.month]
+        delta1 = datetime.timedelta(days=91)
+        tasks = []
+        for task in user_tasks:
+            date2 = datetime.datetime.now() + delta1
+            print(date2, task.data)
+            if task.data < date2 and task.check is False:
+                tasks.append(task)
     elif options == "3 Month":
         delta1 = datetime.timedelta(days=91)
         tasks = []
         for task in user_tasks:
-            date2 = task.data - delta1
-            if task.data < date2:
+            date2 = datetime.datetime.now() + delta1
+            print(date2, task.data)
+            if task.data < date2 and task.check is False:
                 tasks.append(task)
     elif options == "6 Month":
         delta1 = datetime.timedelta(days=182)
         tasks = []
         for task in user_tasks:
-            date2 = task.data - delta1
-            if task.data < date2:
+            date2 = datetime.datetime.now() + delta1
+            print(date2, task.data)
+            if task.data < date2 and task.check is False:
                 tasks.append(task)
     elif options == "Year":
         delta1 = datetime.timedelta(days=365)
         tasks = []
         for task in user_tasks:
-            date2 = task.data - delta1
-            if task.data < date2:
+            date2 = task.data + delta1
+            print(date2, task.data)
+            if task.data < date2 and task.check is False:
                 tasks.append(task)
 
         # tasks = [task for task in user_tasks]
 
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks, form=form,
+                           url="user_todo_list_sorted_by_periods", options=options)
 
 
 @app.route("/list_by_status/<options>")
 @login_required
 def user_todo_list_sorted_by_states(options):
     user_tasks = current_user.task
+    form = ChangeStatusForm()
     print(options)
     if options not in LIST_OF_STATES:
         return redirect(url_for("logout"))
@@ -168,7 +209,8 @@ def user_todo_list_sorted_by_states(options):
     else:
         tasks = [task for task in user_tasks if task.check is False]
 
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks, form=form,
+                           url="user_todo_list_sorted_by_states", options=options)
 
 
 @app.route("/logout")
